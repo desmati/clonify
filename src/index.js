a157734da4c1ea29238e1b2c99e0d8b85361ac71
Hossein <desmati@gmail.com>
2023-01-09 16:46:35 +0100

    fixed the router problem for discover link

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -8,16 +8,20 @@ import Layout from "./pages/Layout";
 import Library from "./pages/Library";
 import SongPlayList from "./pages/SongPlayList";
 
+import { PlayerContext } from "./utils/playercontext";
+import Songs from "./utils/songs";
+import Body from "./components/body/Body";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
+          <Route index element={<Body />} />
           <Route path="/library" element={<Library />} />
           <Route path="/songplaylist" element={<SongPlayList />} />
-          
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,12 +1,10 @@
-
 import { Outlet, Link } from "react-router-dom";
 import Playlists from "../components/navbar/Playlists";
-
 
 const Library = () => {
   return (
     <>
-    <Playlists/>
+      <Playlists />
     </>
   );
 };
